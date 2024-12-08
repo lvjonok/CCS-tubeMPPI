d928aa229ca78909103eb7b0325544026e0e75f3
Lev Kozlov <kozlov.l.a10@gmail.com>
2024-12-08 12:16:20 +0900

    feat: add jax version of CCS mppi algorithm

--- jax_bike_mppi.py
+++ jax_bike_mppi.py
@@ -5,7 +5,8 @@ from abc import ABC, abstractmethod
 from tqdm import tqdm
 from track2obstacles import ConvexHull, smooth_track, generate_boundaries, add_obstacles
 import matplotlib.pyplot as plt
-from controllers.LinCovSteer import linCovSteer, getMatrices
+from controllers.LinCovSteer import linCovSteer
+from ccs import formulate_problem, solve_problem
 
 
 # define dynamics of bicycle model
@@ -164,16 +165,16 @@ class MPPI(Controller):
         cost += jnp.sum(discounted, axis=1)
         # cost += jnp.sum(distances, axis=1)
 
-        # we want to have direction aligned with the target, more important at the end
-        angles = jnp.arctan2(
-            closest_target[1] - state_sequences[:, :, 1],
-            closest_target[0] - state_sequences[:, :, 0],
-        )
-        angle_weights = jnp.geomspace(0.1, 1.0, state_sequences.shape[1])
-        angle_cost = jnp.sum(
-            angle_weights * jnp.abs(angles - state_sequences[:, :, 2]), axis=1
-        )
-        cost += angle_cost
+        # # we want to have direction aligned with the target, more important at the end
+        # angles = jnp.arctan2(
+        #     closest_target[1] - state_sequences[:, :, 1],
+        #     closest_target[0] - state_sequences[:, :, 0],
+        # )
+        # angle_weights = jnp.geomspace(0.1, 1.0, state_sequences.shape[1])
+        # angle_cost = jnp.sum(
+        #     angle_weights * jnp.abs(angles - state_sequences[:, :, 2]), axis=1
+        # )
+        # cost += angle_cost
 
         # we want to keep average speed positive and close to 3.0
         speed = jnp.mean(state_sequences[:, :, 3], axis=1)
@@ -215,6 +216,65 @@ class MPPI(Controller):
     def controls(self):
         return self.sol
 
+    def nominal_states(self, state0, N=None):
+        # using the controls we have predicted during mppi iteration
+        # we perform rollout to get where we would be
+        # if we followed the predicted controls
+        state = state0
+        states = [state]
+
+        for i in range(N or self.cfg["horizon"]):
+            state = step(state, self.sol[i])
+            states.append(state)
+
+        return jnp.array(states)
+
+
+# @jax.jit
+def getObsConstr(Xlist, N, obstacles, max_obstacles: int):
+    """
+    Function to generate obstacle avoidance constraints for covariance steering
+    """
+    R = 0.15
+    threshold = 0.4
+
+    nx = Xlist[0].shape[0]
+    constrData = []
+    help_points = []
+
+    if max_obstacles == 0:
+        return constrData, help_points
+
+    for k in range(N + 1):
+        Fpos_k = jnp.zeros((2, (N + 1) * nx))
+        Fpos_k = Fpos_k.at[:, k * nx : k * nx + 2].set(jnp.eye(2))
+        # help_points.append([])
+        for obs_tuple in obstacles:
+            # we should consider obstacle if closer than threshold
+            dist = jnp.linalg.norm(Xlist[k][0:2] - obs_tuple)
+            if dist > threshold:
+                continue
+
+            z0 = jnp.array([obs_tuple]).T
+            mu_k_prev = Xlist[k][0:2]
+            deltaz = mu_k_prev - z0
+            norm = jnp.linalg.norm(deltaz, 2)
+            deltaz = deltaz / norm
+            xbark = z0 + deltaz * R
+
+            # help_points[-1].append(xbark)
+
+            a_k = deltaz
+            b_k = a_k.T @ xbark
+
+            atildek = Fpos_k.T @ a_k
+            btildek = b_k
+            constrData.append((atildek, btildek))
+
+            if len(constrData) == max_obstacles:
+                return constrData, help_points
+    return constrData, help_points
+
 
 if __name__ == "__main__":
     # test MPPI controller
@@ -228,7 +288,7 @@ if __name__ == "__main__":
         "act_min": np.array([-1.4, -2.0]),
         "seed": 0,
         # simulation related
-        "N_SIMULATION": 6000,
+        "N_SIMULATION": 600,
         # waypoints related
         # distance to waypoint to accept it
         "accept_waypoint_dist": 0.2,
@@ -238,6 +298,19 @@ if __name__ == "__main__":
         # obstacles parameters
         "obstacle_radius": 0.15,
         "obstacles": None,
+        "obstacles_discretization": 3,
+        # Constrained Covariance Steering
+        "enable_ccs": False,
+        "T_CS": 4,  # number of time steps for CCS to look ahead
+        "Q": np.eye(4) * 1e-4,  # state cost
+        "R": np.eye(2) * 1e-4,  # control cost
+        "dk": np.zeros((4, 1)),  # state deviation
+        "Wk": np.eye(4) * 1e-3,  # state deviation covariance
+        # when after CCS maximum eigenvalue is above this threshold
+        # the nominal state is set equal to the real state
+        # and the covariance is set to 0
+        "sigma_threshold": 1.0,
+        "max_obstacles": 0,
     }
 
     # create random racing track
@@ -269,6 +342,9 @@ if __name__ == "__main__":
     smooth_points = smooth_track(hull_points)
     # Add obstacles along boundaries
     left_boundary, right_boundary = generate_boundaries(smooth_points, TRACK_WIDTH)
+    # take only each 5th point to reduce obstacle density
+    left_boundary = left_boundary[:: config["obstacles_discretization"]]
+    right_boundary = right_boundary[:: config["obstacles_discretization"]]
     config["obstacles"] = jnp.vstack([left_boundary, right_boundary])
 
     # define a set of waypoints
@@ -281,123 +357,155 @@ if __name__ == "__main__":
     theta0 = jnp.arctan2(dir0[1], dir0[0])
     state = np.array([*smooth_points[0], theta0, 0.0])
 
-    mppi.get_action(state)
-    print(mppi.controls)
+    # now we are ready to run the loop for the algorithm
+    state_nominal = state.copy()  # we keep track of the nominal state
+    SigmaK = np.eye(4) * 0  # initial covariance is 0
+    # initialize the controller for the nominal state
+    # and get initial nominal control sequence
+    mppi.get_action(state_nominal)
+    mppi._process_waypoints(state_nominal)
+    controls, controls_nominal = mppi.controls.copy(), mppi.controls.copy()
 
-    # for testing purposes we select a small horizon
-    # inside it given trajectory and controls
-    # we linearize the dynamics and compare
-    # how evolution of the state goes with the linearized dynamics
-    HORIZON_TEST = 10
-
-    state0 = state.copy()
-
-    # perform rollout
-    uref = []
-    state = state0
-    nonlinear_rollout = [state]
-    for i in range(HORIZON_TEST):
-        state = step(state, mppi.controls[i])
-        nonlinear_rollout.append(state)
-        uref.append(mppi.controls[i])
-    nonlinear_rollout = jnp.array(nonlinear_rollout)
-    uref = jnp.array(uref)
-
-    print("Nonlinear rollout")
-    print(nonlinear_rollout[-1])
-
-    # linear rollout
-    linear_rollout = []
-    state = state0
-    Alist = []
-    Blist = []
-    for i in range(HORIZON_TEST):
-        A, B = linearized_dyn(nonlinear_rollout[i], mppi.controls[i])
-        Alist.append(A)
-        Blist.append(B)
-        state = A @ state + B @ mppi.controls[i]
-        linear_rollout.append(state)
-
-    linear_rollout = jnp.array(linear_rollout)
-    print("Linear rollout")
-    print(linear_rollout[-1])
-
-    d = np.zeros((4, 1))
-    W = np.eye(4) * 1e-4
-
-    dlist = [d] * HORIZON_TEST
-    Wlist = [W] * HORIZON_TEST
-
-    mu0 = np.zeros((4, 1))
-    Sigma0 = np.eye(4)
-
-    linCovSteer(
-        Alist,
-        Blist,
-        dlist,
-        Wlist,
-        mu0,
-        Sigma0,
-        Xref=np.expand_dims(nonlinear_rollout, axis=-1),
-        Uref=np.expand_dims(uref, axis=-1),
+    # formulate cvxpy problem for CCS
+    ccs_prob = formulate_problem(
+        nx=4,
+        nu=2,
+        N=config["T_CS"],
+        N_obs=config["max_obstacles"],
     )
 
+    Wk = np.eye(4) * 1e-3
+    Wk[:2, :2] = 0.0
 
-# if __name__ == "__main__":
-#     states_history = jnp.array([state])
+    states_history = jnp.array([state])
+    states_nominal_history = jnp.array([state_nominal])
 
-#     for _ in tqdm(range(config["N_SIMULATION"]), desc="Simulating"):
-#         action = mppi.get_action(state)
-#         state = step(state, action)
-#         states_history = jnp.vstack([states_history, state])
+    for _ in tqdm(range(config["N_SIMULATION"]), desc="Simulating"):
+        # run mppi for the nominal state
+        action = mppi.get_action(state_nominal)
 
-#         mppi._process_waypoints(state)
+        # part about CCS
+        if config["enable_ccs"]:
+            # get nominal trajectory and controls
+            nominal_controls = mppi.controls
+            nominal_states = mppi.nominal_states(state_nominal, N=config["T_CS"])
+            # generate a list of linearized dynamics for the nominal trajectory
+            Alist = []
+            Blist = []
+            for i in range(config["T_CS"]):
+                A, B = linearized_dyn(nominal_states[i], nominal_controls[i])
+                Alist.append(A)
+                Blist.append(B)
 
-#     print("Simulation done.")
-#     fig, ax = plt.subplots(figsize=(10, 6))
-#     ax.plot(
-#         states_history[:, 0],
-#         states_history[:, 1],
-#         label="Trajectory",
-#         color="blue",
-#     )
-#     ax.quiver(
-#         states_history[:-1, 0],
-#         states_history[:-1, 1],
-#         jnp.cos(states_history[:-1, 2]),
-#         jnp.sin(states_history[:-1, 2]),
-#         scale=20,
-#         width=0.003,
-#         color="r",
-#         label="Direction",
-#     )
-#     ax.plot(
-#         smooth_points[:, 0],
-#         smooth_points[:, 1],
-#         color="black",
-#         label="Central Line",
-#         linewidth=2,
-#     )
+            d = np.zeros((4, 1))
+            W = np.eye(4) * 0
 
-#     discretization = 3  # simple const for obstacle density
-#     add_obstacles(
-#         left_boundary,
-#         discretization,
-#         mppi.cfg["obstacle_radius"],
-#         ax,
-#         color="gray",
-#     )
-#     add_obstacles(
-#         right_boundary,
-#         discretization,
-#         mppi.cfg["obstacle_radius"],
-#         ax,
-#         color="gray",
-#     )
+            dlist = [d] * config["T_CS"]
+            Wlist = [W] * config["T_CS"]
 
-#     ax.set_xlabel("X position")
-#     ax.set_ylabel("Y position")
-#     ax.set_title("Bicycle Model Trajectory")
-#     ax.legend()
-#     ax.grid()
-#     plt.show()
+            # compute obstacles constraints for nominal trajectory
+            constrData, _ = getObsConstr(
+                Xlist=nominal_states,
+                N=config["T_CS"],
+                obstacles=config["obstacles"],
+                max_obstacles=config["max_obstacles"],
+            )
+            constrData = [(np.array(a), np.array(b)) for a, b in constrData]
+
+            # have to define Qlist and Rlist
+            Q = np.eye(4) * 1
+            R = np.eye(2) * 1e-2
+            Qlist = [Q] * (config["T_CS"] + 1)
+            Rlist = [R] * config["T_CS"]
+
+            uff_, L_, K_, prob_status = solve_problem(
+                *ccs_prob,
+                Alist=np.array(Alist),
+                Blist=np.array(Blist),
+                dlist=dlist,
+                Wlist=Wlist,
+                mu_0=np.array(state_nominal).reshape(-1, 1),
+                Sigma_0=SigmaK,
+                Qlist=Qlist,
+                Rlist=Rlist,
+                Xref=np.expand_dims(nominal_states[: config["T_CS"] + 1], axis=-1),
+                Uref=np.expand_dims(nominal_controls[: config["T_CS"]], axis=-1),
+                ObsAvoidConst=constrData,
+            )
+            if prob_status != "optimal":
+                print("Optimization failed", prob_status)
+                break
+
+            ubark, Kfbk = uff_[0:2, :], L_[0:2, :]
+            uk = ubark + Kfbk @ np.array(state - state_nominal).reshape(-1, 1)
+
+            # fix the dimensions
+            ubark = ubark.T.flatten()
+            uk = uk.T.flatten()
+            Ak, Bk = linearized_dyn(state_nominal, ubark)
+            Sigmakp1 = ((Ak + Bk @ Kfbk) @ SigmaK @ (Ak + Bk @ Kfbk).T) + Wk
+            SigmaK = Sigmakp1
+        else:
+            # if no CCS we just use the nominal control
+            uk = action
+            ubark = action
+
+        wk = np.random.multivariate_normal(np.zeros(4), Wk, (1,)).flatten() * 0
+
+        # Stepping simulation forward
+        state = step(state, uk) + wk
+        state_nominal = step(state_nominal, ubark)
+
+        # update state and nominal state
+        states_history = jnp.vstack([states_history, state])
+        states_nominal_history = jnp.vstack([states_nominal_history, state_nominal])
+
+        mppi._process_waypoints(state)
+
+        # also CCS part
+        # check if we need to reset the nominal state
+        # if the maximum eigenvalue of the covariance is above the threshold
+        if np.max(np.linalg.eigvals(SigmaK)) > config["sigma_threshold"]:
+            state_nominal = state.copy()
+            SigmaK = np.eye(4) * 0
+
+    print("Simulation done.")
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.plot(
+        smooth_points[:, 0],
+        smooth_points[:, 1],
+        color="black",
+        label="Central Line",
+        linewidth=2,
+        linestyle="--",
+        alpha=0.5,
+    )
+    ax.plot(
+        states_history[:, 0],
+        states_history[:, 1],
+        label="Trajectory",
+        color="blue",
+    )
+
+    add_obstacles(
+        left_boundary,
+        config["obstacles_discretization"],
+        mppi.cfg["obstacle_radius"],
+        ax,
+        color="gray",
+    )
+    add_obstacles(
+        right_boundary,
+        config["obstacles_discretization"],
+        mppi.cfg["obstacle_radius"],
+        ax,
+        color="gray",
+    )
+
+    ax.set_xlabel("X position")
+    ax.set_ylabel("Y position")
+    ax.set_title("Bicycle Model Trajectory")
+    ax.legend()
+    ax.grid()
+    plt.show()
+    print("Done.")
